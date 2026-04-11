c9c696f124e8f6f0443cf0bab545280848d959b9
efratahgarcia-sketch <efratah.garcia@gmail.com>
2026-04-11 12:04:24 -0600

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,38 @@
+console.log("Hola");
+let numero = 3; //
+let texto = "Hola"; //
+let booleano = true; //
+let nulo = null; //
+let indefinido = undefined; //
+console.log(numero);
+console.log(texto);
+console.log(booleano);
+console.log(nulo);
+console.log(indefinido);
+let x = 5; //
+let y = 10; //
+let suma = x+y; //
+let resta = x-y; //
+let potencia = x**y; //
+let incremento = x++; //
+let decremento = y--; //
+console.log(x+y);
+console.log(suma);
+console.log(x==y);
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x!=y);
+var a = 10;
+if (a>5) {
+    var a =20;
+    console.log(a);
+}
+console.log(a);
+let b = 10;
+if(b>5) {
+    let b = 20;
+    console.log(b);
+}
+console.log(b);
