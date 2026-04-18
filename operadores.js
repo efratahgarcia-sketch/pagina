eca0dd93443bc4e432ece97081b6fefa430c8942
efratahgarcia-sketch <efratah.garcia@gmail.com>
2026-04-18 12:17:32 -0600

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,82 @@
+let x = 10;
+let y = 15;
+console.log(x+y);
+let a = 45;
+let b = 45;
+console.log(a+b);
+let c = 8;
+let d = 160;
+console.log(c*d);
+let e = 34;
+let f = 250;
+console.log(e-f);
+let g = 4;
+let h = 8;
+console.log(g**h);
+let i = 5000;
+let j = 87;
+console.log(i/j);
+let k = 2;
+let l = 3;
+console.log(k**l);
+var num = -5;
+if (num > 0){console.log("El número es positivo");
+    }else{console.log("Es negativo");
+    }
+let edad = 25;
+if (edad<18){
+    console.log("No puedes entrar al club");
+}else if (edad >= 18 && edad <21){
+    console.log("Puedes entrar al club, pero no beber");
+}else{
+    console.log("Puedes entrar al club y beber");
+}
+let m = 5;
+let n = 7;
+console.log((m*n)/2);
+var num = 11
+switch (num){
+    case 1:
+        console.log("lunes");
+        break;
+    case 2:
+        console.log("martes");
+        break;
+    case 3:
+        console.log("miércoles");
+        break;
+    case 4:
+        console.log("jueves");
+        break;
+    case 5:
+        console.log("viernes");
+        break;
+    case 6:
+        console.log("sábado");
+        break;
+    case 7:
+        console.log("domingo");
+        break;
+    default:
+        console.log("Número inválido");
+}
+var num = 17
+if (num%2==0){
+    console.log("El número es par");
+}else{
+    console.log("El número es impar");
+}
+var color = "azul"
+switch (color){
+    case "verde":
+        console.log("Avanza");
+        break;
+    case "amarillo":
+        console.log("Detente");
+        break;
+    case "rojo":
+        console.log("Mantente en alto");
+        break;
+    default:
+        console.log("Color inválido");
+}
